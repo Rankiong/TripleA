cf48fa551d6282507f3b2ecab4bc54dfd397cc20
Kaos <Rankiong@outlook.es>
2018-08-10 13:53:13 +0200

    Sala Movediza V1

---
+++ Assets/Kevin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kevin : MonoBehaviour {
+
+    private void Awake()
+    {
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            float tiempo = Time.time;
+            Debug.Log(tiempo);
+        }
+    }
+
+    public void Tiempo()
+    {
+        float tempo = Time.deltaTime;
+        Debug.Log(tempo);
+
+        if (tempo <= 2)
+            Debug.Log("Hola");
+        if (tempo >= 3 && tempo <= 4)
+            Debug.Log("Venga");
+        if (tempo >= 5)
+            Debug.Log("Pues ya estaria");
+    }
+
+    private void Start()
+    {
+        Tiempo();
+    }
+
+    private void Update()
+    {
+        //Debug.Log(tiempo);
+    }
+}
---
+++ Assets/Scripts/SalaMovediza.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalaMovediza : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine("EliminacionDeBaldosas");
+    }
+
+    public void Comprobador()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            StartCoroutine("EliminacionDeBaldosas");
+        }
+
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            GameObject[] goes = GameObject.FindGameObjectsWithTag("BaldosasPinchos");
+            Debug.Log(goes.Length);
+            for (int i = 0; i < 128; i++)
+            {
+                goes[i].SetActive(true);
+            }
+        }
+    }
+
+    IEnumerator EliminacionDeBaldosas()
+    {
+        for (int i = 0; i < 128; i++)
+        {
+            GameObject[] goes = GameObject.FindGameObjectsWithTag("BaldosasPinchos");
+            goes[Random.Range(0, goes.Length)].SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void Update()
+    {
+        Comprobador();
+    }
+}
