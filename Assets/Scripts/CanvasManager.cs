8b45b4c86ba8204b1d490b36127ecd1e4a0724f1
Kaos <Rankiong@outlook.es>
2018-08-12 02:20:27 +0200

    TextMeshPro, GameManager y creacion de objeto y avance en el menu principal.

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+    public GameObject SubPanelSonido;
+    public GameObject SubPanelGraficos;
+    public GameObject SubPanelCrosshair;
+    public TMP_Dropdown DropdownGraficos;
+
+    //Pone TRUE el SubPanel de opciones de sonido
+    public void OnSubPanelSonido()
+    {
+        SubPanelSonido.SetActive(true);
+        SubPanelGraficos.SetActive(false);
+        SubPanelCrosshair.SetActive(false);
+    }
+
+    //Pone TRUE el SubPanel de opciones graficas
+    public void OnSubPanelGraficos()
+    {
+        SubPanelGraficos.SetActive(true);
+        SubPanelSonido.SetActive(false);
+        SubPanelCrosshair.SetActive(false);
+    }
+
+    //Pone TRUE el SubPanel de opciones del Crosshair
+    public void OnSubPanelCrosshair()
+    {
+        SubPanelCrosshair.SetActive(true);
+        SubPanelSonido.SetActive(false);
+        SubPanelGraficos.SetActive(false);
+    }
+
+    //Cambia las opciones graficas del juego
+    public void CambiandoGraficos()
+    {
+        if (DropdownGraficos.value == 0)
+        {
+            QualitySettings.SetQualityLevel(0);
+        }
+        if (DropdownGraficos.value == 1)
+        {
+            QualitySettings.SetQualityLevel(1);
+        }
+        if (DropdownGraficos.value == 2)
+        {
+            QualitySettings.SetQualityLevel(2);
+        }
+        if (DropdownGraficos.value == 3)
+        {
+            QualitySettings.SetQualityLevel(3);
+        }
+        if (DropdownGraficos.value == 4)
+        {
+            QualitySettings.SetQualityLevel(4);
+        }
+        if (DropdownGraficos.value == 5)
+        {
+            QualitySettings.SetQualityLevel(5);
+        }
+
+    }
+
+    //Comprobador de nivel grafico
+    public void ComprobadorGrafico()
+    {
+        Debug.Log("El nivel grafico se encuentra en: " + QualitySettings.GetQualityLevel());
+        DropdownGraficos.value = QualitySettings.GetQualityLevel();
+    }
+
+    private void Update()
+    {
+        ComprobadorGrafico();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Camera CamaraPlayer;
+    public GameObject PanelSinMateriales;
+    public LayerMask mask;
+    public float RangoRaycast;
+    int ContadorDeCubos = 0;
+    GameObject ObjetoPerdido = null;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    //Elimina objetos por filtro
+    public void EliminarObjetos()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(CamaraPlayer.transform.position,CamaraPlayer.transform.forward, out hit, RangoRaycast, mask))
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                Destroy(hit.transform.gameObject);
+                ContadorDeCubos++;
+            }
+        }
+    }
+
+    //Crea objetos con filtro
+    public void CrearObjetos()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(CamaraPlayer.transform.position, CamaraPlayer.transform.forward, out hit, RangoRaycast))
+        {
+            if(Input.GetButtonDown("Fire2"))
+            {
+                if(ContadorDeCubos > 0)
+                {
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    go.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    go.AddComponent<Rigidbody>().mass = 100;
+                    go.layer = 8;
+                    //go.transform.position = hit.transform.position + hit.transform.forward;
+                    go.transform.position = hit.point + hit.transform.forward;
+                    ObjetoPerdido = go;
+                    ContadorDeCubos--;
+                }
+                else if(ContadorDeCubos <= 0)
+                {
+
+                }
+            }
+        }
+    }
+
+    //Sistema de seguridad para devolver el ultimo objeto creado
+    public void EliminarUltimoObjeto()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (ObjetoPerdido != null)
+            {
+                Destroy(ObjetoPerdido);
+                ObjetoPerdido = null;
+                ContadorDeCubos++;
+            }
+        }
+    }
+
+    //Corrutina de advertencia cuando no tienes materiales
+    IEnumerator SinMateriales()
+    {
+        PanelSinMateriales.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        PanelSinMateriales.SetActive(false);
+    }
+
+    private void Update()
+    {
+        EliminarObjetos();
+        CrearObjetos();
+        EliminarUltimoObjeto();
+    }
+}
