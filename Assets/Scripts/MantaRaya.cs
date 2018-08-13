9fa6c171857b2193eb8893b7c128b8998b94fea0
Demluraz <josealfonsomt@gmail.com>
2018-08-13 12:40:04 +0200

    Deathzones puestas en inicio

---
+++ Assets/Scripts/MantaRaya.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MantaRaya : MonoBehaviour {
+
+    public Vector3 posicioninicial;
+    public Transform jugador;
+    
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            jugador.position = posicioninicial;
+        }    
+    }
+}
---
+++ Assets/Scripts/RayaZone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayaZone : MonoBehaviour
+{
+    public Transform mantaRaya;
+    public Transform followed;
+    public Transform defaultPosition;
+
+    public float velocidad;
+    public LayerMask triggerLayers;
+    private bool dentro;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        dentro = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        dentro = false;
+    }
+
+    private void Update()
+    {
+        Vector3 target = dentro ? followed.position : defaultPosition.position;        
+
+        Vector3 direction = target - mantaRaya.position;
+
+        float remainDistance = direction.magnitude;
+
+        if (remainDistance > 0F)
+        {
+            mantaRaya.rotation = Quaternion.LookRotation(direction);
+
+            float distance = velocidad * Time.deltaTime;
+
+            if (distance > remainDistance)
+                mantaRaya.position = target;
+            else
+                mantaRaya.position += direction.normalized * distance;
+        }
+    }
+}
